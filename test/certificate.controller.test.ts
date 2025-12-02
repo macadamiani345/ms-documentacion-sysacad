cba5910105c3f613b6bc9c1486b79a4e90f8d645
TinchoMerca <martinmercado2004@gmail.com>
2025-12-02 18:45:01 -0300

    carpeta views y test realizadas

---
+++ test/certificate.controller.test.ts
@@ -0,0 +1,132 @@
+
+
+
+import { ServerHTTP } from "../src/index"
+// import { ClassRoomService } from "../../src/services/classRoom.service"
+import { StudentCreateInput} from "../src/validators/student.validator"
+import request from 'supertest'
+
+
+const appTest = ServerHTTP.getInstance()
+
+
+beforeAll(async () => {
+    appTest.start()
+})
+
+beforeEach(async () => {
+    jest.clearAllMocks()
+})
+        
+
+afterAll(async () => {
+    appTest.stop()
+})
+
+
+
+describe("Certificate controller", () => {
+
+
+    describe("POST /certificate/pdf", () => {
+
+
+        test("Should response with a 200 status code and buffer data", async () => {
+
+            const student : StudentCreateInput = {
+                id: 1,
+                apellido: 'Diaz Rossi',
+                nombre: 'Juan Cruz',
+                nro_documento: 45588489,
+                tipo_documento: "DNI",
+                fecha_ingreso: '2022-03-01',
+                sexo: 'M',
+                nro_legajo: 8873,
+                fecha_nacimiento: '2004-01-21'
+            }
+
+            const response = await request(appTest.getApp()).post('/certificate/pdf').send(student)
+
+            expect(response.statusCode).toBe(200)
+
+            expect(response.body).toEqual(expect.any(Buffer))
+
+        })
+
+        
+        test("Should response with a 400 status code and error", async () => {
+
+            const student : any = {
+                id: 1,
+                nombre: 'Juan Cruz',
+                nro_documento: 45588489,
+                tipo_documento: "DNI",
+                fecha_ingreso: '2022-03-01',
+                sexo: 'M',
+                nro_legajo: 8873,
+                fecha_nacimiento: '2004-01-21'
+            }
+
+            const response = await request(appTest.getApp()).post('/certificate/pdf').send(student)
+
+            expect(response.statusCode).toBe(400)
+
+            expect(response.body.error).toBe('Los datos enviados son incorrectos')
+
+        })
+
+    })
+
+
+    describe("POST /certificate/docx", () => {
+
+        test("Should response with a 200 status code and buffer data", async () => {
+
+            const student : StudentCreateInput = {
+                id: 1,
+                apellido: 'Diaz Rossi',
+                nombre: 'Juan Cruz',
+                nro_documento: 45588489,
+                tipo_documento: "DNI",
+                fecha_ingreso: '2022-03-01',
+                sexo: 'M',
+                nro_legajo: 8873,
+                fecha_nacimiento: '2004-01-21'
+            }
+
+            const response = await request(appTest.getApp()).post('/certificate/docx').send(student)
+
+            expect(response.statusCode).toBe(200)
+
+            expect(response.body).toEqual(expect.any(Buffer))
+
+        })
+
+        test("Should response with a 400 status code and error", async () => {
+
+            const student : any = {
+                id: 1,
+                nombre: 'Juan Cruz',
+                nro_documento: 45588489,
+                tipo_documento: "DNI",
+                fecha_ingreso: '2022-03-01',
+                sexo: 'M',
+                nro_legajo: 8873,
+                fecha_nacimiento: '2004-01-21'
+            }
+
+            const response = await request(appTest.getApp()).post('/certificate/docx').send(student)
+
+            expect(response.statusCode).toBe(400)
+
+            expect(response.body.error).toBe('Los datos enviados son incorrectos')
+
+        })
+
+        // test("Should response with a 503 status code and error", async () => {
+
+        // })
+
+    })
+
+})
